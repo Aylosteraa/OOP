50c6320020bc3267911d9d13e7d6ba150c586ee8
Marina Barabash <113929861+Aylosteraa@users.noreply.github.com>
2022-09-22 08:53:46 +0300

    Update lab1_ex1.py

--- lab1_ex1.py
+++ lab1_ex1.py
@@ -1,4 +1,5 @@
 import argparse
+import sys
 
 parser = argparse.ArgumentParser(description="Arithmetic operation")
 parser.add_argument("operation", type=str)
@@ -7,8 +8,7 @@ args = parser.parse_args()
 op = ['+', '-', '*', '/']
 for i in args.operation:
     boo_l = i in op or i.isdigit()
-if boo_l is False:
-    print("Error! This operation can't be calculated.")
-else:
-    print(eval(args.operation))
+    if boo_l is False:
+        sys.exit("Error! This operation can't be calculated.")
+ print(eval(args.operation))
 
