029f94450488fe6a661c72ad617ada6f7f580cd8
Marina Barabash <113929861+Aylosteraa@users.noreply.github.com>
2022-09-24 18:07:36 +0300

    Update lab_1

--- lab1_ex1.py
+++ lab1_ex1.py
@@ -1,14 +1,15 @@
 import argparse
-import sys
 
 parser = argparse.ArgumentParser(description="Arithmetic operation")
+parser.add_argument("int1", type=str)
 parser.add_argument("operation", type=str)
+parser.add_argument("int2", type=str)
 args = parser.parse_args()
 
-op = ['+', '-', '*', '/']
-for i in args.operation:
-    boo_l = i in op or i.isdigit()
-    if boo_l is False:
-        sys.exit("Error! This operation can't be calculated.")
- print(eval(args.operation))
-
+if args.operation in ('+', '-', '*', '/') and args.int1.isdigit() and args.int2.isdigit():
+    if args.operation == '/' and args.int2 == '0':
+        print("Error! Division by zero.")
+    else:
+        print(eval(args.int1 + args.operation + args.int2))
+else:
+    print("Error! This operation can't be calculated.")
