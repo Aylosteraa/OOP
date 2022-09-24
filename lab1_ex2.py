e7c698a62c2cb51a0b33db0aab642c5aed3fb018
Marina Barabash <113929861+Aylosteraa@users.noreply.github.com>
2022-09-24 18:09:11 +0300

    Update lab_1

--- lab1_ex2.py
+++ lab1_ex2.py
@@ -1,18 +1,23 @@
 import argparse
 import operator
+import math
+import sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument("operation", type=str)
 parser.add_argument("integers", type=int, nargs='+')
 args = parser.parse_args()
 
-y = dir(operator)
-if args.operation in y:
+if args.operation in dir(operator):
     x = getattr(operator, args.operation)
+elif args.operation in dir(math):
+    x = getattr(math, args.operation)
+else:
+    sys.exit("Error! This operation can't be calculated.")
+if len(args.integers) == 1:
+    res = x(args.integers[0])
+else:
     res = args.integers[0]
     for i in range(1, len(args.integers)):
-        res = x(args.integers[i], res)
-    print(res)
-else:
-    print("Error! This operation can't be calculated.")
-
+        res = x(res, args.integers[i])
+print(res)
