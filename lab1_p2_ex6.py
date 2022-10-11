4a8b06e5ffcd00fabd1670a0425626cc8a7c47e6
Marina Barabash <113929861+Aylosteraa@users.noreply.github.com>
2022-10-11 10:20:06 +0300

    Update lab1_p2_ex6.py

--- lab1_p2_ex6.py
+++ lab1_p2_ex6.py
@@ -2,41 +2,41 @@ class BINARY_TREE:
     def __init__(self, code, price):
         self.left = None
         self.right = None
-        self.__code = code
-        self.__price = price
+        self.code = code
+        self.price = price
 
     def insert(self, code, price):
-        if self.__code:
-            if code < self.__code:
+        if self.code:
+            if code < self.code:
                 if self.left is None:
                     self.left = BINARY_TREE(code, price)
                 else:
                     self.left.insert(code, price)
-            elif code > self.__code:
+            elif code > self.code:
                 if self.right is None:
                     self.right = BINARY_TREE(code, price)
                 else:
                     self.right.insert(code, price)
         else:
-            self.__code = code
-            self.__price = price
+            self.code = code
+            self.price = price
 
     def find_code(self, code, quantity):
-        if code < self.__code:
+        if code < self.code:
             if self.left is None:
-                return str(code) + " is not Found"
+                return f'{code} is not Found'
             return self.left.find_code(code, quantity)
-        elif code > self.__code:
+        elif code > self.code:
             if self.right is None:
-                return str(code) + " is not Found"
+                return f'{code} is not Found'
             return self.right.find_code(code, quantity)
         else:
-            return self.__price * quantity
+            return self.price * quantity
 
     def print_tree(self):
         if self.left:
             self.left.print_tree()
-        print("Product (", self.__code, "): ", self.__price)
+        print("Product (", self.code, "): ", self.price)
         if self.right:
             self.right.print_tree()
 
