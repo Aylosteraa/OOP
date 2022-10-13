789f9bc46391bbdbd96ff396156607cebf88dcf5
Marina Barabash <113929861+Aylosteraa@users.noreply.github.com>
2022-10-13 09:05:37 +0300

    Update lab1_p2_ex1.py

--- lab1_p2_ex1.py
+++ lab1_p2_ex1.py
@@ -1,6 +1,17 @@
 class Rectangle:
-    length = 1.0
-    width = 1.0
+
+
+    def __init__(self, x=1, y=1):
+        if not isinstance(x, float | int):
+            raise TypeError("Length is float-point number")
+        if not isinstance(y, float | int):
+            raise TypeError("Width is float-point number")
+        if not 0 < x < 20:
+            raise ValueError("Length is number larger than 0.0 and less than 20.0")
+        if not 0 < y < 20:
+            raise ValueError("Width is numbers larger than 0.0 and less than 20.0")
+        self.length = x
+        self.width = y
 
     def setter(self, x, y):
         if not isinstance(x, float | int):
@@ -24,7 +35,10 @@ class Rectangle:
         return self.length * self.width
 
 
-rect = Rectangle()
+rect = Rectangle(12, 14.1)
+print(rect.getter())
+print(rect.perimeter())
+print(rect.area())
 rect.setter(10, 2.3)
 print(rect.getter())
 print(rect.perimeter())
